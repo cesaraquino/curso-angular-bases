093f3e2a2c376d79938c00295ff2d3d6a0843703
Cesar Aquino <cesaraquino@gmail.com>
2024-12-28 20:31:12 -0500

    fixes

--- src/app/counter/components/counter/counter.component.ts
+++ src/app/counter/components/counter/counter.component.ts
@@ -8,12 +8,13 @@ import { Component } from '@angular/core';
     <button (click)="increaseBy(+1)">+1</button>
     <button (click)="resetCounter()">Reset</button>
     <button (click)="increaseBy(-1)">-1</button>
-  `,
+
+  `
 })
-export class CounterComponent {
+export class CounterComponent  {
   public counter: number = 10;
 
-  increaseBy(value: number): void {
+  increaseBy( value: number ):void {
     this.counter += value;
   }
 
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -5,8 +5,16 @@ import { HeroComponent } from './hero/hero.component';
 import { ListComponent } from './list/list.component';
 
 @NgModule({
-  exports: [HeroComponent, ListComponent],
-  declarations: [HeroComponent, ListComponent],
-  imports: [CommonModule],
+  exports: [
+    HeroComponent,
+    ListComponent,
+  ],
+  declarations: [
+    HeroComponent,
+    ListComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
 })
-export class HeroesModule {}
+export class HeroesModule { }
